701660d0544619719fa04d9724c66e1cabe3c46e
Lukas <lukiza2002@gmail.com>
2025-06-28 21:29:32 +0200

    Player script refactor

--- src/Player/player.gd
+++ src/Player/player.gd
@@ -1,59 +1,93 @@
 extends CharacterBody2D
 
-
+# Movement settings
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-@onready var anim = $PlayerAnimation
-var is_attacking = false
+# Stamina settings
+const MAX_STAMINA = 16
+const STAMINA_REGEN_RATE = 1.0
+const ATTACK_COST = 5.0
 
-# Stamina
-var max_stamina = 16
-var current_stamina = max_stamina
-var stamina_regen_rate = 1.0
-var attack_stamina_cost = 5.0
+# Nodes
+@onready var player_animation = $PlayerAnimation
+@onready var player_sprite = $PlayerAnimatedSprite
 @onready var stamina_bar = $Camera2D/StaminaBarUI
 
-func _process(delta: float) -> void:
-	if current_stamina < max_stamina and not is_attacking:
-		current_stamina = min(current_stamina + stamina_regen_rate * delta, max_stamina)
-		stamina_bar.update_stamina(current_stamina, max_stamina)
+# States
+var current_stamina = MAX_STAMINA
+var is_attacking = false
+var is_jumping = false
 
-func _ready():
-	anim.connect("animation_finished", _on_animation_finished)
-	stamina_bar.update_stamina(current_stamina, max_stamina)
-	
-func _on_animation_finished(anim_name):
-	if anim_name == "Attack":
-		is_attacking = false
+func _ready() -> void:
+	player_animation.animation_finished.connect(_on_animation_finished)
+	stamina_bar.update_stamina(current_stamina, MAX_STAMINA)
+
+func _process(delta: float) -> void:
+	regenerate_stamina(delta)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	apply_gravity(delta)
+	handle_movement()
+	handle_attack()
+	move_and_slide()
+			
+func apply_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+func handle_movement() -> void:
+	if is_attacking:
+		return
+		
+	var direction := Input.get_axis("ui_left", "ui_right")
+	handle_horizontal_movement(direction)
+	handle_jump()
+	update_movement_animation(direction)
 
-	# Handle jump.
+func handle_horizontal_movement(direction: float) -> void:
+	if direction:
+		velocity.x = direction * SPEED
+		player_sprite.flip_h = direction < 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+func handle_jump() -> void:
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		is_jumping = true
 		
-	# Handle attack
-	if Input.is_action_pressed("ui_accept") and current_stamina >= attack_stamina_cost and not is_attacking:
-		current_stamina -= attack_stamina_cost
-		stamina_bar.update_stamina(current_stamina, max_stamina)
+func update_movement_animation(direction: float) -> void:
+	if direction != 0:
+		player_animation.play("Run")
+	elif is_on_floor():
+		player_animation.play("Idle")
+	else:
+		player_animation.play("Jump")
+		
+func handle_attack() -> void:
+	if Input.is_action_just_pressed("ui_accept") and can_attack() and not is_attacking:
+		consume_stamina(ATTACK_COST)
 		is_attacking = true
-		anim.play("Attack")
-	
-	if not is_attacking:
-		var direction := Input.get_axis("ui_left", "ui_right")
-		if direction == -1:
-			get_node("PlayerAnimatedSprite").flip_h = true
-		elif direction == 1:
-			get_node("PlayerAnimatedSprite").flip_h = false
-		if direction:
-			velocity.x = direction * SPEED
-			anim.play("Run")
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			anim.play("Idle")
+		player_animation.play("Attack")
+
+func can_attack() -> bool:
+	return current_stamina >= ATTACK_COST
+
+func consume_stamina(amount: float) -> void:
+	current_stamina = max(current_stamina - amount, 0)
+	stamina_bar.update_stamina(current_stamina, MAX_STAMINA)
+
+func regenerate_stamina(delta: float) -> void:
+	if not is_attacking and current_stamina < MAX_STAMINA:
+		current_stamina = min(current_stamina + STAMINA_REGEN_RATE * delta, MAX_STAMINA)
+		stamina_bar.update_stamina(current_stamina, MAX_STAMINA)
+
+func _on_animation_finished(anim_name):
+	match anim_name:
+		"Attack":
+			is_attacking = false
+		"Jump":
+			is_jumping = false
 
 	move_and_slide()
