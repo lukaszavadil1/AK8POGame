aaa1b81ce1810955fb71cceab912f60d780f1346
Lukas <lukiza2002@gmail.com>
2025-06-28 18:14:29 +0200

    Added menu and level 1 background + attack animation

--- src/main.gd
+++ src/main.gd
@@ -6,4 +6,4 @@ func _on_quit_button_pressed() -> void:
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://level_1.tscn")
+	get_tree().change_scene_to_file("res://Levels/level_1.tscn")
