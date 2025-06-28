e639a3625d42b533f0186e1690581b4798cfb514
Lukas <lukiza2002@gmail.com>
2025-06-28 19:08:37 +0200

    Extended player actions to support attack + changed global player jump and attack controls

--- src/Player/player.gd
+++ src/Player/player.gd
@@ -4,7 +4,15 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-@onready var anim = get_node("PlayerAnimation")
+@onready var anim = $PlayerAnimation
+var is_attacking = false
+
+func _ready():
+	anim.connect("animation_finished", _on_animation_finished)
+	
+func _on_animation_finished(anim_name):
+	if anim_name == "Attack":
+		is_attacking = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,21 +20,25 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction == -1:
-		get_node("PlayerAnimatedSprite").flip_h = true
-	elif direction == 1:
-		get_node("PlayerAnimatedSprite").flip_h = false
-	if direction:
-		velocity.x = direction * SPEED
-		anim.play("Run")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		anim.play("Idle")
+		
+	# Handle attack
+	if Input.is_action_pressed("ui_accept"):
+		is_attacking = true
+		anim.play("Attack")
+	
+	if not is_attacking:
+		var direction := Input.get_axis("ui_left", "ui_right")
+		if direction == -1:
+			get_node("PlayerAnimatedSprite").flip_h = true
+		elif direction == 1:
+			get_node("PlayerAnimatedSprite").flip_h = false
+		if direction:
+			velocity.x = direction * SPEED
+			anim.play("Run")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			anim.play("Idle")
 
 	move_and_slide()
