7ee080a31163c73af2b76135c35f95cedbc5fd39
JosephSusik <joseph.susik@gmail.com>
2025-06-30 22:07:20 +0200

    working enemy idle, hurt, death animations

--- src/Enemies/enemy_1.gd
+++ src/Enemies/enemy_1.gd
@@ -5,14 +5,19 @@ var current_health = MAX_HEALTH
 var is_dead = false
 const GRAVITY = 1200.0 
 
-@onready var enemy_animation = $EnemyAnimation
+@onready var enemy_animation = $Enemy1Sprite2D
 @onready var sprite = $Enemy1Sprite2D
 @onready var health_bar = $HealthBar
 
 func _ready() -> void:
 	update_health_bar()
 	enemy_animation.play("Idle")
+	enemy_animation.animation_finished.connect(_on_animation_finished)
 
+func _on_animation_finished() -> void:
+	# You can't get the animation name here, so check the current animation manually
+	if enemy_animation.animation == "Hurt" and not is_dead:
+		enemy_animation.play("Idle")
 
 func take_damage(amount: int) -> void:
 	if is_dead:
