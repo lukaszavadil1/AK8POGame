547880f7ab78fe758393e49fe5257c869e4b3101
Lukas <lukiza2002@gmail.com>
2025-07-02 22:01:41 +0200

    Level switch hotfix

--- src/Utils/game_state.gd
+++ src/Utils/game_state.gd
@@ -4,10 +4,11 @@ var current_level: int = 1
 const TOTAL_LEVELS: int = 5
 
 func get_next_level_path() -> String:
+	current_level += 1
 	if is_final_level():
 		return "res://Scenes/Main.tscn"
 	else:
-		return "res://Scenes/Levels/level_%d.tscn" % (current_level + 1)
+		return "res://Scenes/Levels/level_%d.tscn" % current_level
 
 func reset() -> void:
 	current_level = 1
